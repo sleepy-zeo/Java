3e11c5962398b72abc10f721fdf69d2f8efd7206
wuhongwei <hongwei.wu@grandsun.com>
2020-11-09 10:43:09 +0800

    Lifecycle framework established

--- src/com/sleepy/zeo/Lifecycle.java
+++ src/com/sleepy/zeo/Lifecycle.java
@@ -9,12 +9,11 @@ package com.sleepy.zeo;
  */
 public interface Lifecycle {
 
-    public static final String BEFORE_START = "before_start";
-    public static final String AFTER_START = "after_start";
-    public static final String BEFORE_STOP = "before_stop";
-    public static final String AFTER_STOP = "after_stop";
-
     void addLifecycleListener(LifecycleListener listener);
+
+    void removeLifecycleListener(LifecycleListener listener);
+
+    void fireLifecycleEvent(String type, Object data);
 
     LifecycleListener[] getLifecycleListeners();
 
--- src/com/sleepy/zeo/LifecycleEvent.java
+++ src/com/sleepy/zeo/LifecycleEvent.java
@@ -2,6 +2,11 @@ package com.sleepy.zeo;
 
 public class LifecycleEvent {
 
+    public static final String TYPE_BEFORE_START = "before_start";
+    public static final String TYPE_AFTER_START = "after_start";
+    public static final String TYPE_BEFORE_STOP = "before_stop";
+    public static final String TYPE_AFTER_STOP = "after_stop";
+
     private Lifecycle lifecycle;
     private String type;
     private Object data;
